19b0745f72a05ee5066ca1907e08ba53fa0c7f73
Helbert Nunes <helbertnunes97@gmail.com>
2022-01-12 01:19:48 -0300

    implementação do botão de comprar jogo

--- corrida-canetas-app/src/app/home/home.module.ts
+++ corrida-canetas-app/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -12,7 +13,10 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule    
+  ],
+  providers:[
+    InAppBrowser
   ],
   declarations: [HomePage]
 })
