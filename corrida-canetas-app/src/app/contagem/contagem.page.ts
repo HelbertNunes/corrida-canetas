74aecd026464f76d1da0baca8b3aca52f7e8a67e
Helbert Nunes <helbertnunes97@gmail.com>
2022-05-14 18:49:36 -0300

    correção do bug da contagem

--- corrida-canetas-app/src/app/contagem/contagem.page.ts
+++ corrida-canetas-app/src/app/contagem/contagem.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { Router, NavigationEnd } from '@angular/router';
+import { Router } from '@angular/router';
 import { NativeAudio } from '@awesome-cordova-plugins/native-audio/ngx';
+
 
 @Component({
   selector: 'app-contagem',
@@ -10,45 +11,56 @@ import { NativeAudio } from '@awesome-cordova-plugins/native-audio/ngx';
 })
 export class ContagemPage implements OnInit {
 
+  body:HTMLElement;
+  text:HTMLElement;
+  button:HTMLElement;
+
   constructor(
     public platform: Platform,
     private router: Router,
     private nativeAudio: NativeAudio
-  ) {
-    this.router.events.subscribe((e) => {
-      if (e instanceof NavigationEnd) {        
-        this.carregaContagem();
-      }
-    });
-  }
+  ) { }
 
   async ngOnInit() {
     this.nativeAudio.preloadSimple('somContagem', '../../assets/sounds/Pop 2.mp3');
     this.nativeAudio.preloadSimple('somJa', '../../assets/sounds/Big Bell.mp3');
+    this.body = document.getElementById("fundo");
+    this.text = document.getElementById("text");
+    this.button = document.getElementById("btPontuacao");
+  }
+
+  async ionViewWillEnter(){
+    await this.carregaContagem(); 
+  }
+
+  async ionViewDidLeave(){
+    this.body.style.backgroundColor = '#e83c47';
+    this.text.textContent = '3'
+    this.button.style.display = 'block';
   }
 
   async carregaContagem() {
-    const body = document.getElementById("fundo");
-    const texto = document.getElementById("text");
-    const botao = document.getElementById("btPontuacao");
     await this.delay(500);
     this.nativeAudio.play('somContagem', () => console.log('somContagem is done playing'));
-    body.style.backgroundColor = '#e83c47';
-    texto.textContent = '3'
+    this.body.style.backgroundColor = '#e83c47';
+    this.text.textContent = '3'
     await this.delay(1000);
     this.nativeAudio.play('somContagem', () => console.log('somContagem is done playing'));
-    body.style.backgroundColor = '#F6A022';
-    texto.textContent = '2'
+    this.body.style.backgroundColor = '#F6A022';
+    this.text.textContent = '2'
     await this.delay(1000);
     this.nativeAudio.play('somContagem', () => console.log('somContagem is done playing'));
-    body.style.backgroundColor = '#64A83E';
-    texto.textContent = '1'
+    this.body.style.backgroundColor = '#64A83E';
+    this.text.textContent = '1'
     await this.delay(1000);
     this.nativeAudio.play('somJa', () => console.log('somContagem is done playing'));
-    body.style.backgroundColor = '#3C8ACC';
-    texto.textContent = 'JÁ!'
-    
-    botao.style.display = 'block';
+    this.body.style.backgroundColor = '#3C8ACC';
+    this.text.textContent = 'JÁ!'
+    this.button.style.display = 'block';
+  }
+
+  irParaPontuacao(){
+    this.router.navigate(['pontuacao']);
   }
 
   delay(ms: number) {
