a17fbbe537df21dc2fe2d4c109871462035ea1a4
Helbert Nunes <helbertnunes97@gmail.com>
2023-06-06 00:38:33 -0300

    botão e ação de abrir manual

--- corrida-canetas-app/src/app/home/home.page.ts
+++ corrida-canetas-app/src/app/home/home.page.ts
@@ -9,4 +9,8 @@ export class HomePage {
   abreSiteCompra(){
     window.open("https://www.playeasy.com.br/corrida-de-canetas.html")
   }
+  abreManual(){
+    window.open("https://www.dropbox.com/s/umy0h1icglk2l2y/manual_corrida_v2.0.pdf?dl=0")
+  }
+  
 }
